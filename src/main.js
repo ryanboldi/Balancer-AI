04c965444d9407ac947bb47f4034ed937947045f
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-05-10 23:36:55 +0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
-function setup() {
+const WIDTH = 800;
+const HEIGHT = 800;
 
+//bunch of module aliases
+let Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+let env;
+
+function setup() {
+    //RECTANGLES NOW DEFINED BY THIER CENTER POINT.
+    rectMode(CENTER);
+    createCanvas(WIDTH, HEIGHT);
+    env = new Environment(0, 0, WIDTH, HEIGHT);
 }
 
 function draw() {
+    background(190);
+    env.draw();
 
 }
